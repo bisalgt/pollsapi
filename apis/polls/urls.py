3d8f9078c83accec9ca080425f745d6cdb00282b
Bishal <bisalgt@gmail.com>
2019-11-11 14:44:31 +0545

    creating views with apiview

--- apis/polls/urls.py
+++ apis/polls/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from apis.polls import views
+from apis.polls import apiviews
 
 urlpatterns = [
     path("polls/",views.polls_list, name="polls_list"),
-    path("polls/<int:id>/",views.polls_detail, name="polls_detail"),
+    path("poll/<int:id>/",views.polls_detail, name="polls_detail"),
+    path("polls_api/",apiviews.PollList.as_view(), name="polls_api_list"),
+    path("poll_api/<int:id>/",apiviews.PollDetail.as_view(), name="poll_api_detail"),
 ]
