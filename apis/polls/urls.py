0a27358de491fcb7f7dbdd51520c8b067ae26a6b
Bishal <bisalgt@gmail.com>
2019-11-18 10:09:45 +0545

    access control and authentication using token

--- apis/polls/urls.py
+++ apis/polls/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-
+from rest_framework.authtoken import views as authview
 
 from apis.polls import views
 from apis.polls import apiviews
@@ -13,12 +13,15 @@ urlpatterns = [
     path("polls/",views.polls_list, name="polls_list"),
     path("poll/<int:id>/",views.polls_detail, name="polls_detail"),
     path("polls_api/",apiviews.PollList.as_view(), name="polls_api_list"),
-    path("poll_api/<int:id>/",apiviews.PollDetail.as_view(), name="poll_api_detail"),
+    path("poll_api/<int:pk>/",apiviews.PollDetail.as_view(), name="poll_api_detail"),
     path("polls_api/<int:pk>/choices/<int:choice_pk>/vote/",apiviews.CreateVoteAPIView.as_view(), name="vote_api"),
     path("polls_generic_api/",generic_apiviews.PollListAPIView.as_view(), name="polls_generic_api_list"),
     path("poll_generic_api/<int:pk>/",generic_apiviews.PollDetailAPIView.as_view(), name="poll_generic_api_detail"),
     path("polls_generic_api/<int:pk>/choices/", generic_apiviews.ChoiceListAPIView.as_view(), name="choices_generic_api_list"),
     path("polls_generic_api/<int:pk>/choices/<int:choice_pk>/vote/", generic_apiviews.CreateVoteAPIView.as_view(), name="votes_generic_api"),
+    path("users/", generic_apiviews.UserCreateAPIView.as_view(), name="users"),
+    path("login/", generic_apiviews.LoginView.as_view(), name="login"),
+    path("login_rest/", authview.obtain_auth_token, name="login_rest"),
 ]
 
 urlpatterns += router.urls
